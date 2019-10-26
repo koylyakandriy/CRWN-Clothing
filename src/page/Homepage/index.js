ac6157d83c9e0e6e4502d934703393f97cb1ee99
koylyakandriy <koylyakandriy@gmail.com>
2019-10-26 09:48:05 +0300

    create new app and few components and and router

---
+++ src/page/Homepage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Directory from "../../components/Directory";
+
+import "./HomePage.scss";
+
+const HomePage = () => {
+	return (
+		<div className="homepage">
+			<Directory/>
+		</div>
+	);
+};
+
+export default HomePage;
